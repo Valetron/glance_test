d89ec183c48cf80a6c2bf1b8fca0643d76abc00f
Veler Jevsky <43819843+Valetron@users.noreply.github.com>
2021-07-15 02:44:20 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Gla
+{
+    public partial class Form1 : Form
+    {
+        private string path;
+        private int curIndexImage = 0;
+        private string[] images;
+
+        public Form1()
+        {
+            InitializeComponent();
+            images = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.png");
+        }
+    }
+}
